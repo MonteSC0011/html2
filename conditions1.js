9797b43908c6ae086c9110858d1a8a785d7fb160
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-06-07 15:29:48 -0700

    JS

---
+++ conditions1.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+let randomNumber = Math.random(); //0.0-1
+
+// if (randomNumber < 0.50) {
+
+//     console.log("Condition was Met");
+//     console.log(randomNumber);
+
+// }
+
+if (randomNumber >= 0.5) {
+
+    console.log('Condition Was Met');
+    console.log(randomNumber); 
+}
+
--- domWalkthrough1.js
+++ domWalkthrough1.js
@@ -144,9 +144,11 @@ console.log(titles);
 
 titles[0].textContent = 'Hey Monte, I changed the title again';
 
+// 
 var odd = document.querySelectorAll('li:nth-child(odd)');
 var even = document.querySelectorAll('li:nth-child(even)');
 
+// 
 for (var i = 0; i < odd.length; i++) {
 
     odd[i].style.backgroundColor = 'grey';

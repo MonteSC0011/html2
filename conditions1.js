8eb323a38f675beca63fe9602a0a1c96b5830394
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-06-07 15:37:41 -0700

    JS

--- conditions1.js
+++ conditions1.js
@@ -2,13 +2,14 @@ console.log("Hello World");
 
 let randomNumber = Math.random(); //0.0-1
 
-// if (randomNumber < 0.50) {
+if (randomNumber < 0.50) {
 
-//     console.log("Condition was Met");
-//     console.log(randomNumber);
+    console.log("Condition was Met");
+    console.log(randomNumber);
 
-// }
+}
 
+// 
 if (randomNumber >= 0.5) {
 
     console.log('Condition Was Met');

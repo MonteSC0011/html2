2635e1c05a2290593361404dcf6851111fffbc12
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-06-08 14:35:21 -0700

    JS

--- conditions1.js
+++ conditions1.js
@@ -16,19 +16,37 @@
 //     console.log(randomNumber); 
 // }
 
-const dayOfWeek = 'Saturday';
+// 
+// const dayOfWeek = 'Saturday';
 
+// if (dayOfWeek === 'Monday'){
+//     console.log("Yay its Monday!");
 
-if (dayOfWeek === 'Monday'){
+// } else if (dayOfWeek === 'Friday'){
 
-    console.log("Yay its Monday!");
-    
-} else if (dayOfWeek === 'Friday'){
+//     console.log("Yay Its Friday");
 
-    console.log("Yay Its Friday");
+// } else if (dayOfWeek === 'Saturday') {
 
-}else if (dayOfWeek === 'Saturday') {
+//     console.log("You have the correct Day from the variable"); 
+// }
 
-    console.log("You have the correct Day from the variable"); 
+const age = prompt("Enter An Age");
 
+if (age < 5) {
+
+    console.log("Hey, you're a baby!");
+
+} else if (age < 10) {
+
+    console.log("Hey, you're just a kid!");
+} else if (age < 35) {
+
+    console.log("You're getting older.");
 }
+// IF ALL OTHER CONDITIONS FAIL - ELSE.
+else {
+
+    console.log("You are over the age of 35.");
+}
+

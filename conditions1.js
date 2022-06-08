8395c9b9d25968c6cb184a5638f5178f3ba6f5b3
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-06-08 14:28:23 -0700

    JS

--- conditions1.js
+++ conditions1.js
@@ -1,18 +1,34 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-let randomNumber = Math.random(); //0.0-1
+// let randomNumber = Math.random(); //0.0-1
 
-if (randomNumber < 0.50) {
+// if (randomNumber < 0.50) {
 
-    console.log("Condition was Met");
-    console.log(randomNumber);
+//     console.log("Condition was Met");
+//     console.log(randomNumber);
+
+// }
+
+// // 
+// if (randomNumber >= 0.5) {
+
+//     console.log('Condition Was Met');
+//     console.log(randomNumber); 
+// }
+
+const dayOfWeek = 'Saturday';
+
+
+if (dayOfWeek === 'Monday'){
+
+    console.log("Yay its Monday!");
+    
+} else if (dayOfWeek === 'Friday'){
+
+    console.log("Yay Its Friday");
+
+}else if (dayOfWeek === 'Saturday') {
+
+    console.log("You have the correct Day from the variable"); 
 
 }
-
-// 
-if (randomNumber >= 0.5) {
-
-    console.log('Condition Was Met');
-    console.log(randomNumber); 
-}
-
